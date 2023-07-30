caee4bcdbcccb50d7e69aa3169e805fa6b7ae939
Keith Anderson <keith.anderson@campbellsci.com>
2023-07-29 20:31:44 -0600

    jhkug \

---
+++ c#/stuff.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace CSharpTutorials
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string message = "Hello World!!";
+
+            Console.WriteLine(message);
+        }
+    }
+}
