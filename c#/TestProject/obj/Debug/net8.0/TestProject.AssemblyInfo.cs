043eed0aab89bb62f0f3c2fe0b5dcbc30f514d2f
Keith Anderson <keith.anderson@campbellsci.com>
2023-09-01 07:34:20 -0600

    Notebook

--- c#/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ c#/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d61ae72f420d7e3f7a8f6f07472e235a7f96456c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fed4f67ef9a81a92dc5abd4c1b1a0a76cae9c87a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
